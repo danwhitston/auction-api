aea10b1606677cd8d6f49937158c345d97dd8395
Daniel Whitston <dan@whitston.org.uk>
2022-03-31 17:09:54 +0100

    Remove errant console.log

--- api-app/middleware/validateQuery.js
+++ api-app/middleware/validateQuery.js
@@ -1,6 +1,5 @@
 // Based on https://gist.github.com/stongo/6359042?permalink_comment_id=3476052#gistcomment-3476052
 module.exports = (validator) => {
-  console.log("query middleware!");
   return (req, res, next) => {
     const { error } = validator(req.query);
     if (error) {
